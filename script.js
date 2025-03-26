177b9bebd28945c80ebac672a24aa44f59fc26f4
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2025-03-26 12:29:46 +0100

    Forgot to add and commit the hamburgermenu, done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('✅ JavaScript is running!');
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById('navbar').style.width = '100%';
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById('navbar').style.width = '0%';
+}
